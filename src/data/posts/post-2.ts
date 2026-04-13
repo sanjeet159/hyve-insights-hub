c1e2828216a7f0e257ea7823e9a093160bf8876d
sanjeet159 <84581508+sanjeet159@users.noreply.github.com>
2026-04-13 13:44:01 +0530

    Update post-2.ts

--- src/data/posts/post-2.ts
+++ src/data/posts/post-2.ts
@@ -21,7 +21,7 @@ const post: BlogPostWithContent = {
   author: "Sanjeet Kumar",
   date: "Apr 10, 2026",
   readTime: "7 min read",
-  image: blog-1.jpg,
+  image: blog-1,
   featured: false,
   content: `## How to Hire a Freelance Team in India for Your Startup
 
