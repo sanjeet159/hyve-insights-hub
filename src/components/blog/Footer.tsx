d5fc3c49e4cc25af4f00c22673c9e057429a1403
sanjeet159 <84581508+sanjeet159@users.noreply.github.com>
2026-04-13 12:51:04 +0530

    Update Footer.tsx

--- src/components/blog/Footer.tsx
+++ src/components/blog/Footer.tsx
@@ -176,7 +176,7 @@ const Footer = () => {
                   <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
                   <path d="M22 4L12 14.01l-3-3" />
                 </svg>
-                Thanks for subscribing! ✅
+                Thanks for subscribing! 
               </motion.div>
             )}
           </div>
