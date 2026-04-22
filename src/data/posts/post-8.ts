065677963c2ac2448742075a04bcc16e206b407d
sanjeet159 <84581508+sanjeet159@users.noreply.github.com>
2026-04-22 18:08:48 +0530

    Update post-8.ts

--- src/data/posts/post-8.ts
+++ src/data/posts/post-8.ts
@@ -1,4 +1,4 @@
-import blogFeatured from "@/assets/blog-featured.jpg";
+import blog8 from "@/assets/blog-8.jpg";
 import type { BlogPostWithContent } from "./types";
 
 const post: BlogPostWithContent = {
@@ -21,7 +21,7 @@ const post: BlogPostWithContent = {
   author: "Sanjeet Kumar",
   date: "Apr 18, 2026",
   readTime: "10 min read",
-  image: blogFeatured,
+  image: blog8,
   imagePosition: "center",
   featured: false,
   content: `<div style="font-family:inherit;line-height:1.8;color:#333;max-width:100%">
